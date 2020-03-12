d5dd797d2dcb8af0d1485211199dc3d25003ee88
R7 <richard.tsang9641@gmail.com>
2020-03-12 23:52:32 +0800

    Updates

---
+++ precache-manifest.a9fa1958e6672383851615230222b3f8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56f92a8c9e2f6efb6bfd466cbf2c8a3f",
+    "url": "//r7chardgh.github.io/react-gh-pages-demo/index.html"
+  },
+  {
+    "revision": "41893a44523dee222030",
+    "url": "//r7chardgh.github.io/react-gh-pages-demo/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "e26ec8b2b5737ab9ea8a",
+    "url": "//r7chardgh.github.io/react-gh-pages-demo/static/js/2.e02c83ff.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "//r7chardgh.github.io/react-gh-pages-demo/static/js/2.e02c83ff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "41893a44523dee222030",
+    "url": "//r7chardgh.github.io/react-gh-pages-demo/static/js/main.0799745a.chunk.js"
+  },
+  {
+    "revision": "a2f0825f9401c301f195",
+    "url": "//r7chardgh.github.io/react-gh-pages-demo/static/js/runtime-main.cd312b30.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "//r7chardgh.github.io/react-gh-pages-demo/static/media/logo.5d5d9eef.svg"
+  }
+]);
